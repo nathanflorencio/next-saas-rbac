53dd7999a6b0e674185b537bbeeb159e0fcaf443
Nathan Florencio <nflorencio13@gmail.com>
2024-04-28 01:30:11 -0300

    delete a project route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -26,6 +26,7 @@ import { shutdownOrganization } from '@/http/routes/orgs/shutdown-organization'
 import { transferOrganization } from '@/http/routes/orgs/transfer-organization'
 import { updateOrganization } from '@/http/routes/orgs/update-organization'
 import { createProject } from '@/http/routes/projects/create-project'
+import { deleteProject } from '@/http/routes/projects/delete-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -80,6 +81,7 @@ app.register(shutdownOrganization)
 app.register(transferOrganization)
 
 app.register(createProject)
+app.register(deleteProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
