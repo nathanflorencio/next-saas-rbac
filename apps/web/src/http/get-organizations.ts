845d4a8cb034e40a240c06984fd02909feadc926
Nathan Florencio <nflorencio@showcasepro.com.br>
2024-06-13 23:37:31 -0300

    feat: fetching organizations in home page

---
+++ apps/web/src/http/get-organizations.ts
@@ -0,0 +1,16 @@
+import { api } from '@/http/api-client'
+
+interface GetOrganizationsResponse {
+  organizations: {
+    id: string
+    name: string
+    slug: string
+    avatarUrl: string | null
+  }[]
+}
+
+export async function getOrganizations() {
+  const result = await api.get('organizations').json<GetOrganizationsResponse>()
+
+  return result
+}
