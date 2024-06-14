0bd1da20568856ea46da3864632fad6b8f286ae9
Nathan Florencio <nflorencio@showcasepro.com.br>
2024-06-13 21:55:18 -0300

    chore: create authenticate with github in frontend

--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 
+import { signInWithGithub } from '@/app/auth/actions'
 import { signInWithEmailAndPassword } from '@/app/auth/sign-in/actions'
 import githubIcon from '@/assets/github-icon.svg'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
@@ -25,64 +26,67 @@ export function SignInForm() {
   )
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
-      {success === false && message && (
-        <Alert variant="destructive">
-          <AlertTriangle className="size-4" />
-          <AlertTitle>Sign in failed!</AlertTitle>
-          <AlertDescription>
-            <p>{message}</p>
-          </AlertDescription>
-        </Alert>
-      )}
-
-      <div className="space-y-1">
-        <Label htmlFor="email">E-mail</Label>
-        <Input name="email" type="email" id="email" />
-
-        {errors?.email && (
-          <p className="text-xs font-medium text-red-500 dark:text-red-400">
-            {errors.email[0]}
-          </p>
-        )}
-      </div>
-
-      <div className="space-y-1">
-        <Label htmlFor="password">Password</Label>
-        <Input name="password" type="password" id="password" />
-
-        {errors?.password && (
-          <p className="text-xs font-medium text-red-500 dark:text-red-400">
-            {errors.password[0]}
-          </p>
+    <div className="space-y-4">
+      <form onSubmit={handleSubmit} className="space-y-4">
+        {success === false && message && (
+          <Alert variant="destructive">
+            <AlertTriangle className="size-4" />
+            <AlertTitle>Sign in failed!</AlertTitle>
+            <AlertDescription>
+              <p>{message}</p>
+            </AlertDescription>
+          </Alert>
         )}
 
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          Forgot your password?
-        </Link>
-      </div>
+        <div className="space-y-1">
+          <Label htmlFor="email">E-mail</Label>
+          <Input name="email" type="email" id="email" />
 
-      <Button type="submit" className="w-full" disabled={isPending}>
-        {isPending ? (
-          <Loader2 className="animate-sping size-4" />
-        ) : (
-          'Sign in with e-mail'
-        )}
-      </Button>
+          {errors?.email && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.email[0]}
+            </p>
+          )}
+        </div>
 
-      <Button variant="link" className="w-full" size="sm" asChild>
-        <Link href="/auth/sign-up">Create new account</Link>
-      </Button>
+        <div className="space-y-1">
+          <Label htmlFor="password">Password</Label>
+          <Input name="password" type="password" id="password" />
 
+          {errors?.password && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.password[0]}
+            </p>
+          )}
+
+          <Link
+            href="/auth/forgot-password"
+            className="text-xs font-medium text-foreground hover:underline"
+          >
+            Forgot your password?
+          </Link>
+        </div>
+
+        <Button type="submit" className="w-full" disabled={isPending}>
+          {isPending ? (
+            <Loader2 className="animate-sping size-4" />
+          ) : (
+            'Sign in with e-mail'
+          )}
+        </Button>
+
+        <Button variant="link" className="w-full" size="sm" asChild>
+          <Link href="/auth/sign-up">Create new account</Link>
+        </Button>
+      </form>
       <Separator />
 
-      <Button type="submit" variant="outline" className="w-full">
-        <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
-        Sign in with GitHub
-      </Button>
-    </form>
+      <form action={signInWithGithub}>
+        <Button type="submit" variant="outline" className="w-full">
+          <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
+          Sign in with GitHub
+        </Button>
+      </form>
+    </div>
   )
 }
