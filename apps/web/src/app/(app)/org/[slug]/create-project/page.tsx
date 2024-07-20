71025cce26dd06a485e56ed5f9d910acb25e7a60
Nathan Florencio <nflorencio@showcasepro.com.br>
2024-07-20 02:22:21 -0300

    feat: implementing navlink component

--- apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from 'next/navigation'
 
 import { ProjectForm } from '@/app/(app)/org/[slug]/create-project/project-form'
 import { ability } from '@/auth/auth'
-import { Header } from '@/components/header'
 
 export default async function CreateProject() {
   const permissions = await ability()
@@ -12,13 +11,10 @@ export default async function CreateProject() {
   }
 
   return (
-    <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px] space-y-4">
-        <h1 className="text-2xl font-bold">Create project</h1>
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold">Create project</h1>
 
-        <ProjectForm />
-      </main>
+      <ProjectForm />
     </div>
   )
 }
