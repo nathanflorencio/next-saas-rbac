1fe39cb56c845825b942dfceabe39f97e1f5bf9c
Nathan Florencio <nflorencio@showcasepro.com.br>
2024-07-20 19:48:17 -0300

    feat: implementing project list page and RSC

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
@@ -21,6 +22,7 @@ const buttonVariants = cva(
       },
       size: {
         default: 'h-10 px-4 py-2',
+        xs: 'h-6 rounded-md px-2 text-xs',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
